7767d0b440953e2d5198011bc26ac8a3f449571a
Andre Jarboe <andrejarboe@gmail.com>
2023-01-10 17:48:44 -0500

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+	const [count, setCount] = useState(0);
+	const [text, setText] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('hello');
+	};
+
 	return (
-		<>
-			<h2>lorem ipsum project setup</h2>
-		</>
+		<section className="section-center">
+			<h3>Tired of boring lorem ipum?</h3>
+			<form className="lorem-form" onSubmit={handleSubmit}>
+				<label htmlFor="amount">paragraphs:</label>
+				<input
+					type="number"
+					name="amount"
+					id="amount"
+					value={count}
+					onChange={(e) => setCount(e.target.value)}
+				/>
+				<button type="submit" className="btn">
+					generate
+				</button>
+			</form>
+
+				<article className="lorem-text">
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis velit
+						voluptate laborum, sint veniam et aliquid doloremque dolor nisi,
+						debitis sed eaque laudantium ad labore obcaecati, numquam culpa illo
+						praesentium?
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis velit
+						voluptate laborum, sint veniam et aliquid doloremque dolor nisi,
+						debitis sed eaque laudantium ad labore obcaecati, numquam culpa illo
+						praesentium?
+					</p>
+        <p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis velit
+						voluptate laborum, sint veniam et aliquid doloremque dolor nisi,
+						debitis sed eaque laudantium ad labore obcaecati, numquam culpa illo
+						praesentium?
+					</p>
+				</article>
+		</section>
 	);
 }
 
